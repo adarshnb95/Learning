60311e1e840560a0094700c39d7c48ebff0ceb3c
Adarsh Bhandary <adarshnb95@gmail.com>
2022-09-27 20:13:34 -0500

    Adding 2 more files for python beginners

---
+++ PythonFiles/ArithmeticOperators.py
@@ -0,0 +1,21 @@
+x = 10
+y = 3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x//y)
+print(x%y)
+print((x+y)*x)
+#Until we add a message, type remains as int. The moment we add a string, we need to perform operations separately.
+
+# Comparison operators
+
+a = 3 > 2
+print(a) # Will print True as 3 is greater than 2
+#Similarly we have >=, <, <=, ==, !=
+
+price = 25
+print(price > 10 and price < 30) # And operators can be used for between comparisons.
+
---
+++ PythonFiles/stringFuncs.py
@@ -0,0 +1,8 @@
+course = "Python for beginners"
+print(course.upper())
+print(course.lower())
+print(course.find("y"))
+print(course.find("Y"))  #Python is case sensitive, so Y wont be found and will return -1
+print(course.replace("for", "4"))
+print("Python" in course)  #Returns boolean
+ 
