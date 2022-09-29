3046f93b36b76fe9fd3921c95d4948923584a00f
Adarsh Bhandary <adarshnb95@gmail.com>
2022-09-29 18:30:44 -0500

    ignoring log files

---
+++ SQLFiles/TestingSQL.sql
@@ -0,0 +1,21 @@
+CREATE TABLE employee(
+    employee_ID INTEGER, 
+    first_name VARCHAR(255), 
+    last_name VARCHAR(255), 
+    gender VARCHAR(255), 
+    position VARCHAR(255), 
+    department_ID INTEGER, 
+    salary INTEGER);
+
+INSERT INTO employee
+VALUES 
+    (2002, "Super", "Man", "M", "Tester", 1, 75000),
+    (2003, "Jessica", "Liyers", "F", "Architect", 1, 60000),
+    (2004, "Bonnie", "Adams", "F", "Project Manager", 80000),
+    (2005, "James", "Madison", "M", "Software Developer", 1, 55000),
+    (2006, "Michael", "Greenback", "M", "Sales Assistant", 2, 85000),
+    (2007, "Leslie", "Peters", "F", "Sales Engineer", 2, 76000),
+    (2008, "Max", "Powers", "M", "Sales Representative", 2, 59000),
+    (2009, "Stacy", "Jacobs", "F", "Sales Manager", 2, 730000),
+    (2010, "John", "Henery", "M", "Sales Director", 2, 90000)
+
